a05b6c32ff8817918a2e89d1eccbef6b0a1c06c4
dopecello <williamlocke.cello@gmail.com>
2022-09-12 19:08:45 -0400

    created put route for voting on a post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,12 @@
+const sequelize = require("../../config/connection");
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Vote } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
     const dbPostData = await Post.findAll({
       attributes: ["id", "post_url", "title", "created_at"],
-      order: [['created_at', 'DESC']], 
+      order: [["created_at", "DESC"]],
       include: [
         {
           model: User,
@@ -65,7 +66,7 @@ router.put("/:id", async (req, res) => {
     const dbPostData = Post.update(
       {
         title: req.body.title,
-        post_url: req.body.post_url
+        post_url: req.body.post_url,
       },
       {
         where: {
@@ -82,6 +83,38 @@ router.put("/:id", async (req, res) => {
     console.log(err);
     res.status(500).json(err);
   }
+});
+
+router.put("/upvote", (req, res) => {
+  Vote.create({
+    user_id: req.body.user_id,
+    post_id: req.body.post_id,
+  }).then(() => {
+    // then find the post we just voted on
+    return Post.findOne({
+      where: {
+        id: req.body.post_id,
+      },
+      attributes: [
+        "id",
+        "post_url",
+        "title",
+        "created_at",
+        // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
+        [
+          sequelize.literal(
+            "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+          ),
+          "vote_count",
+        ],
+      ],
+    })
+      .then((dbPostData) => res.json(dbPostData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
 });
 
 router.delete("/:id", async (req, res) => {
