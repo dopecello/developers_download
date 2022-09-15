da7d06dbb2c7e896bf88c8db43908df7c6f345ba
dopecello <williamlocke.cello@gmail.com>
2022-09-15 17:00:30 -0400

    tested out a single post template using handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.render("homepage");
+  res.render("homepage", {
+    id: 1,
+    post_url: "https://www.amazon.com",
+    title: "Bulma: How do you center a button in a box?",
+    created_at: new Date(),
+    vote_count: 10,
+    comments: [{}, {}],
+    user: {
+      username: "test_user",
+    },
+  });
 });
 
 module.exports = router;
