97a0b37ec3c53ed2f7fe54ccdb19d03d349743f9
dopecello <williamlocke.cello@gmail.com>
2022-09-18 15:48:31 -0400

    update BCS 2

--- public/javascript/register.js
+++ public/javascript/register.js
@@ -1,14 +1,12 @@
-async function signupFormHandler(event) {
+function signupFormHandler(event) {
   event.preventDefault();
 
   const username = document.querySelector("#username-register").value.trim();
   const email = document.querySelector("#email-register").value.trim();
   const password = document.querySelector("#password-register").value.trim();
 
-  console.log(username, email, password)
-
-  if (username && email && password) {
-    await fetch("/api/users", {
+if (username && email && password) {
+     fetch("/api/users", {
       method: "post",
       body: JSON.stringify({
         username,
@@ -17,8 +15,8 @@ async function signupFormHandler(event) {
       }),
       headers: { "Content-Type": "application/json" },
     }).then((response) => {
-      console.log(response);
-    });
+      console.log(response)
+    })
   }
 }
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = require("./controllers/");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
+const path = require("path");
 const hbs = exphbs.create({});
 
 const app = express();
@@ -12,6 +13,7 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
